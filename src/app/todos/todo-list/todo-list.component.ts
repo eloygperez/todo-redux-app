0e8ff657aa412f183292b42170666f14fb8def16
eloy.gperez <eloy.gperez@gmail.com>
2020-11-10 17:34:10 +0100

    Primer commit

---
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filtrosValidos } from 'src/app/filtro/filtro.actions';
+import { AppState } from '../../../app/app.reducer';
+import { Todo } from '../models/todo.model';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  todos: Todo[] = [];
+  filtroActual: filtrosValidos;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    /* this.store.select('todos')
+      .subscribe(todos => this.todos = todos); */
+
+    // llega el state, pero hace destructuración para acceder
+    // directamente a todos y filtro
+    this.store.subscribe(({ todos, filtro}) => {
+      this.todos = todos;
+      this.filtroActual = filtro;
+    });
+  }
+
+}
